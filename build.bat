0fec9e62143e63ccd54d413b8601d7bbc83c05db
Kroc Camen <kroc@camendesign.com>
2020-09-08 21:10:04 +0100

    Rename ".asm" files to ".wla" Add VSCode build task

--- build.bat
+++ build.bat
@@ -9,28 +9,28 @@ CLS
 ECHO:
 ECHO * assemble source code:
 ECHO =======================
-ECHO - assemble "sms.asm"
-%WLAZ80% %OPTIONS% -i -o "build/sms.o" 		"src/lib/sms.asm"
-ECHO - assemble "ram.asm"
-%WLAZ80% %OPTIONS% -i -o "build/ram.o" 		"src/ram.asm"
-ECHO - assemble "irqs.asm"
-%WLAZ80% %OPTIONS% -i -l "build/irqs.lib"	"src/irqs.asm"
-ECHO - assemble "orig.asm"
-%WLAZ80% %OPTIONS% -i -l "build/orig.lib"	"src/orig.asm"
-ECHO - assemble "blocks.asm"
-%WLAZ80% %OPTIONS% -i -o "build/blocks.o" 	"src/blocks.asm"
-ECHO - assemble "sonic_the_hedgehog.asm"
-%WLAZ80% %OPTIONS% -i -o "build/sonic_the_hedgehog.o" "src/sonic_the_hedgehog.asm"
+ECHO - assemble "sms.wla"
+%WLAZ80% %OPTIONS% -i -o "build/sms.o" 		"src/lib/sms.wla"
+ECHO - assemble "ram.wla"
+%WLAZ80% %OPTIONS% -i -o "build/ram.o" 		"src/ram.wla"
+ECHO - assemble "irqs.wla"
+%WLAZ80% %OPTIONS% -i -l "build/irqs.lib"	"src/irqs.wla"
+ECHO - assemble "orig.wla"
+%WLAZ80% %OPTIONS% -i -l "build/orig.lib"	"src/orig.wla"
+ECHO - assemble "blocks.wla"
+%WLAZ80% %OPTIONS% -i -o "build/blocks.o" 	"src/blocks.wla"
+ECHO - assemble "sonic_the_hedgehog.wla"
+%WLAZ80% %OPTIONS% -i -o "build/sonic_the_hedgehog.o" "src/sonic_the_hedgehog.wla"
 
-ECHO - assemble "sound.asm"
-%WLAZ80% %OPTIONS% -i -l "build/sound.lib" "src/sound.asm"
+ECHO - assemble "sound.wla"
+%WLAZ80% %OPTIONS% -i -l "build/sound.lib" "src/sound.wla"
 
 IF ERRORLEVEL 1 (
     ECHO ! Error
     ECHO:
     GOTO:EOF
 )
-ECHO - link "sonic_the_hedgehog.sms"
+ECHO - link "sonic_the_hedgehog.wla"
 %WLALINK% -S "link_original.ini" "build/sonic_the_hedgehog.sms"
 
 IF ERRORLEVEL 1 (
